c1f12b409345b0da3149379f091707656de5187f
Ziyan Zhong <202283760004@nuist.edu.cn>
2026-03-30 21:48:29 +0800

    26.3.30

--- scripts/dont_have_tags.gd
+++ scripts/dont_have_tags.gd
@@ -1,12 +1,12 @@
 extends CheckButton
 
 
-func _ready() -> void:
-	button_pressed = MainManager.get_config_value("dont_have_tags", true)
+# func _ready() -> void:
+# 	button_pressed = MainManager.get_config_value("dont_have_tags", true)
 
 
 
 
 func _on_toggled(toggled_on: bool) -> void:
-	SignalBus.save_config.emit("dont_have_tags", toggled_on)
+	# SignalBus.save_config.emit("dont_have_tags", toggled_on)
 	SignalBus.toggle_show_cards_dont_have_tags.emit(toggled_on)
